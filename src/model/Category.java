926773e7ed22ece52a71bac803f31e12c3d2f441
Wilant Nackaerts <wilant.nackaerts@gmail.com>
2018-11-22 13:48:24 +0100

    started on category

--- src/model/Category.java
+++ src/model/Category.java
@@ -1,4 +1,37 @@
 package model;
 
 public class Category {
+    private int id;
+    private String name;
+    private String description;
+
+    public Category(int id, String name, String description) {
+        setId(id);
+        setName(name);
+        setDescription(description);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/model/db/Database.java
+++ src/model/db/Database.java
@@ -1,4 +1,16 @@
 package model.db;
 
+import model.Category;
+
+import java.util.List;
+
 public interface Database {
+
+    void add(Category category);
+
+    Category get(int id);
+
+    void delete(int id);
+
+    List<Category> getAll();
 }
