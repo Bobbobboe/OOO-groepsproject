9998391dda58b4b9244bf13153fe8ba7e434bfc1
Wilant Nackaerts <wilant.nackaerts@gmail.com>
2018-11-23 16:03:29 +0100

    updated controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,38 +1,26 @@
 package controller;
 
-<<<<<<< Updated upstream
-public class Controller {
-
-=======
-<<<<<<< HEAD
-import javafx.collections.ObservableList;
 import model.Category;
-import model.db.Database;
-import model.db.DatabaseText;
+import model.CategoryFactory;
+import model.DomainExeption;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
 
-    private ObservableList<Category> categories;
+    private List<Category> categories = new ArrayList<>();
 
-    public Controller() {
-
-
+    public void enrollCategory(String name, String description, Category category) throws DomainExeption {
+        CategoryFactory factory = new CategoryFactory();
+        Category c = factory.createCategory(name,description,category);
+        addCategory(c);
     }
 
-    public ObservableList<Category> loadData() {
-      return categories;
+    public void addCategory(Category category) throws DomainExeption {
+        if(category == null) throw new DomainExeption();
+        categories.add(category);
     }
 
-    public void addCategorys() {
-        Database db = new DatabaseText();
-        db.fillListQuestions();
-        categories.addAll(db.getAll());
-    }
-=======
-public class Controller {
-
->>>>>>> master
->>>>>>> Stashed changes
 }
+
