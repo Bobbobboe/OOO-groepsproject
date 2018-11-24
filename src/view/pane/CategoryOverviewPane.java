bc5a8c6655787651f4ebc24fd6ce5069c1194365
Kwinnieprince <kwinten@delrue.com>
2018-11-24 16:47:30 +0100

    Optimized imports and set TODO

--- src/view/pane/CategoryOverviewPane.java
+++ src/view/pane/CategoryOverviewPane.java
@@ -5,23 +5,23 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import model.Category;
-import model.MainCategory;
 import model.db.Database;
 import model.db.DatabaseText;
-
-import java.util.List;
 
 
 public class CategoryOverviewPane extends GridPane {
 	private TableView table;
 	private Button btnNew;
-	
-	public CategoryOverviewPane() {
+
+    public CategoryOverviewPane() {
         Database db = new DatabaseText();
 		this.setPadding(new Insets(5, 5, 5, 5));
         this.setVgap(5);
@@ -40,13 +40,13 @@ public class CategoryOverviewPane extends GridPane {
         descriptionCol.setCellValueFactory(new PropertyValueFactory("description"));
         table.getColumns().add(descriptionCol);
 		this.add(table, 0, 1, 2, 6);
-		
 		btnNew = new Button("New");
 		this.add(btnNew, 0, 11, 1, 1);
 	}
-	
+
+	//TODO Where does newAction come from?
 	public void setNewAction(EventHandler<ActionEvent> newAction) {
-		btnNew.setOnAction(newAction);
+        btnNew.setOnAction(newAction);
 	}
 	
 	public void setEditAction(EventHandler<MouseEvent> editAction) {
